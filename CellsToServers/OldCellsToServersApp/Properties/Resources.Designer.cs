06e58798b119f40100906fb510a5c181bb212ffb
szalaigj <szalai@complex.elte.hu>
2016-04-12 19:02:30 +0200

    Rename CellsToServersApp to OldCellsToServersApp.

--- CellsToServers/OldCellsToServersApp/Properties/Resources.Designer.cs
+++ CellsToServers/OldCellsToServersApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CellsToServersApp.Properties {
+namespace OldCellsToServersApp.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CellsToServersApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CellsToServersApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OldCellsToServersApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
