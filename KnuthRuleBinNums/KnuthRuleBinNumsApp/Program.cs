de3be9d5c27ea787a7841d6466c6a8a8099a84a2
szalaigj <szalai@complex.elte.hu>
2016-05-08 15:58:41 +0200

    Extend application with knee point finding. Upgrade package MathNet.Numerics.

--- KnuthRuleBinNums/KnuthRuleBinNumsApp/Program.cs
+++ KnuthRuleBinNums/KnuthRuleBinNumsApp/Program.cs
@@ -4,6 +4,8 @@ using KnuthRuleBinNumsApp.Utils;
 using System.Text;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using MathNet.Numerics;
 
 namespace KnuthRuleBinNumsApp
 {
@@ -14,6 +16,8 @@ namespace KnuthRuleBinNumsApp
             InputParser inputParser = new InputParser();
             HistogramComputer histogramComputer = new HistogramComputer();
             KnuthPosteriorComputer knuthPosteriorComputer = new KnuthPosteriorComputer();
+            LMethod lMethod = new LMethod();
+            FurthestPointFromLineMethod furthestPointFromLineMethod = new FurthestPointFromLineMethod();
             string filename;
             int spaceDimension, pointNO;
             double[] minElems, maxElems;
@@ -21,8 +25,13 @@ namespace KnuthRuleBinNumsApp
                 out minElems, out maxElems);
             int serverNO = inputParser.parseServerNO();
             double posteriorSum = 0.0;
-            double[] posteriorValues = new double[(int)(Math.Sqrt(pointNO)) - 1];
-            for (int histogramResolution = 2; histogramResolution <= Math.Sqrt(pointNO); histogramResolution++)
+            int upperBoundOfPostVals = (int)(Math.Sqrt(pointNO));
+            //int upperBoundOfPostVals = 100;
+            double[] posteriorValues = new double[upperBoundOfPostVals - 1];
+            // The central difference approximations of second derivative of posterior:
+            double[] centralDiffs = new double[upperBoundOfPostVals - 1];
+            int initialHistRes = 2;
+            for (int histogramResolution = initialHistRes; histogramResolution <= upperBoundOfPostVals; histogramResolution++)
             {
                 int[] binHefts = histogramComputer.buildHistogram(histogramResolution, data, spaceDimension, 
                     minElems, maxElems);
@@ -30,15 +39,53 @@ namespace KnuthRuleBinNumsApp
                 writeOutInputSizes(inputParser, filename, spaceDimension, histogramResolution, serverNO, binHefts);
                 double posteriorValue = knuthPosteriorComputer.evaluate(Math.Pow(histogramResolution, spaceDimension), 
                     pointNO, binHefts);
-                posteriorValues[histogramResolution - 2] = posteriorValue;
-                Console.WriteLine("Histogram resolution: {0}, posterior value: {1}", 
-                    histogramResolution, posteriorValue);
+                int idx = histogramResolution - initialHistRes;
+                posteriorValues[idx] = posteriorValue;
+                determineCentralDiff(upperBoundOfPostVals, posteriorValues, centralDiffs, initialHistRes, histogramResolution, idx);
+                Console.WriteLine("Histogram resolution: {0}, posterior value: {1}, central diff: {2}", 
+                    histogramResolution, posteriorValue, centralDiffs[idx]);
                 posteriorSum += posteriorValue;
             }
+            Console.WriteLine("Posterior mean: {0}", (posteriorSum) / ((double)posteriorValues.Length));
+            int maxCentralDiffIdx = determineMaxCentralDiffIdx(pointNO, centralDiffs);
+            Console.WriteLine("Maximum central difference: {0}", maxCentralDiffIdx + initialHistRes);
+            double[] xData = Enumerable.Range(initialHistRes, upperBoundOfPostVals - 1).Select(x => (double)x).ToArray();
+            int kneePointIdx = lMethod.findBestKneePoint(xData, posteriorValues, xData.Length);
+            Console.WriteLine("Knee point (L method): {0}", kneePointIdx + initialHistRes);
+            kneePointIdx = lMethod.iterativeRefinementOfTheKnee(xData, posteriorValues);
+            Console.WriteLine("Refined knee point (L method): {0}", kneePointIdx + initialHistRes);
+            kneePointIdx = furthestPointFromLineMethod.findBestKneePoint(xData, posteriorValues);
+            Console.WriteLine("Knee point ('furthest point from line' method): {0}", kneePointIdx + initialHistRes);
             writeOutPosteriorValues(posteriorValues);
-            Console.WriteLine("Posterior mean: {0}", (posteriorSum) / (Math.Floor(Math.Sqrt(pointNO)) - 1));
             Console.WriteLine("Press any key to continue...");
             Console.Read();
+        }
+
+        private static void determineCentralDiff(int upperBoundOfPostVals, double[] posteriorValues, double[] centralDiffs, int initialHistRes, int histogramResolution, int idx)
+        {
+            if ((histogramResolution == initialHistRes) || (histogramResolution == upperBoundOfPostVals))
+            {
+                centralDiffs[idx] = 0.0;
+            }
+            else
+            {
+                centralDiffs[idx] = posteriorValues[idx - 1] - 2 * posteriorValues[idx] + posteriorValues[idx + 1];
+            }
+        }
+
+        private static int determineMaxCentralDiffIdx(int pointNO, double[] centralDiffs)
+        {
+            int maxCentralDiffIdx = 0;
+            double maxCentralDiff = 0.0;
+            for (int centralDiffIdx = 0; centralDiffIdx < (int)(Math.Sqrt(pointNO)) - 1; centralDiffIdx++)
+            {
+                if (Math.Abs(centralDiffs[centralDiffIdx]) > maxCentralDiff)
+                {
+                    maxCentralDiff = Math.Abs(centralDiffs[centralDiffIdx]);
+                    maxCentralDiffIdx = centralDiffIdx;
+                }
+            }
+            return maxCentralDiffIdx;
         }
 
         private static void writeOutBinHefts(int[] binHefts, int histogramResolution)
