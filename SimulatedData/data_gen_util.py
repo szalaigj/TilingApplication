b4a8509285fe6ebf562e22e04313825d77781b7d
szalaigj <szalai@complex.elte.hu>
2016-05-16 01:43:35 +0200

    Simplify and generalize method multivariate_normal_mixture_9_data_coords.

--- SimulatedData/data_gen_util.py
+++ SimulatedData/data_gen_util.py
@@ -33,37 +33,20 @@ class DataGenUtil:
     coords = np.random.multivariate_normal(mean,cov,self.ndata)
     return coords
 
-  def multivariate_normal_mixture_9_data_coords(self, space_dim, means, 
+  def multivariate_normal_mixture_K_data_coords(self, space_dim, K, means, 
   covs, weights):
-    """
-    This function returns coordinates of generated samples 
-    from multivariate normal mixture distribution from
-    three normal distribution with weights which is a list of
-    three elements
-    """
     coords = np.zeros(shape=(self.ndata,space_dim))
     for idx in range(0,self.ndata):
       selector = np.random.random()
       if (selector < weights[0]):
         coords[idx] = np.random.multivariate_normal(means[0],covs[0])
-      elif ((weights[0] <= selector) and (selector < sum(weights[0:2]))):
-        coords[idx] = np.random.multivariate_normal(means[1],covs[1])
-      elif ((sum(weights[0:2]) <= selector) and (selector < sum(weights[0:3]))):
-        coords[idx] = np.random.multivariate_normal(means[2],covs[2])
-      elif ((sum(weights[0:3]) <= selector) and (selector < sum(weights[0:4]))):
-        coords[idx] = np.random.multivariate_normal(means[3],covs[3])
-      elif ((sum(weights[0:4]) <= selector) and (selector < sum(weights[0:5]))):
-        coords[idx] = np.random.multivariate_normal(means[4],covs[4])
-      elif ((sum(weights[0:5]) <= selector) and (selector < sum(weights[0:6]))):
-        coords[idx] = np.random.multivariate_normal(means[5],covs[5])
-      elif ((sum(weights[0:6]) <= selector) and (selector < sum(weights[0:7]))):
-        coords[idx] = np.random.multivariate_normal(means[6],covs[6])
-      elif ((sum(weights[0:7]) <= selector) and (selector < sum(weights[0:8]))):
-        coords[idx] = np.random.multivariate_normal(means[7],covs[7])
       else:
-        coords[idx] = np.random.multivariate_normal(means[8],covs[8])
+        candidate_idx = 0
+        while ((sum(weights[0:candidate_idx+1]) <= selector) and (candidate_idx+1 < K)):
+          candidate_idx += 1
+        coords[idx] = np.random.multivariate_normal(means[candidate_idx],covs[candidate_idx])
     return coords
-	
+
   def multivariate_uniform_data_coords(self, space_dim, low_value, high_value):
     """
     This function returns coordinates of generated samples 
